b7821a4f07b4fdd7abac3dc4327cc0e2da162453
Maiev <elena.kalichkina.rho@gmail.com>
2020-09-27 17:51:03 +0700

    HomeWork 3

--- main.js
+++ main.js
@@ -1,27 +1,60 @@
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
-let letterF=0;
-let letterS=0;
+const $btn = document.getElementById ('btn-kick');
 
-function getRow(firstRow, secondRow) {
-    for (let i=0; i<firstRow.length; i++) {
-        if (firstRow.charAt(i) == 'а') {
-            letterF++;           
-            }
-    }
-    for (let i=0; i<secondRow.length; i++) {
-        if (secondRow.charAt(i) == 'а') {
-        letterS++;           
-        }
-    }
-    
-    if (letterF > letterS) {
-        console.log(firstRow);
-    }
-    else {
-        console.log(secondRow);
-    }
+const character = {
+    name: 'Pikachu',
+    defaultHP:100,
+    damageHP: 100,
+    elHP: document.getElementById ('health-character'),
+    elProgressBar: document.getElementById ('progressbar-character'),
 }
 
-getRow (firstRow, secondRow);
+const enemy = {
+    name: 'Charmander',
+    defaultHP:100,
+    damageHP: 100,
+    elHP: document.getElementById ('health-enemy'),
+    elProgressBar: document.getElementById ('progressbar-enemy'),
+}
 
+$btn.addEventListener ('click', function () {
+    console.log('Kick!');
+    changeHP (random(20), character);
+    changeHP (random(20), enemy);
+})
+
+function init () {
+    console.log('Start game!');
+    renderHP (character);
+    renderHP (enemy);
+}
+
+function renderHP (person) {
+    renderHPLife (person);
+    renderProgressBar (person);
+}
+
+function renderHPLife (person) {
+    person.elHP.innerText = person.damageHP + '/' + person.defaultHP;
+}
+
+function renderProgressBar (person) {
+    person.elProgressBar.style.width = person.damageHP + '%';
+}
+
+function changeHP (count, person) {
+    if (person.damageHP < count) {
+        person.damageHP =0;
+        alert ('Бедный ' + person.name + ' проиграл!');
+        $btn.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+    
+    renderHP (person);
+}
+
+function random (num) {
+    return Math.ceil(Math.random() * num);
+}
+
+init ();
